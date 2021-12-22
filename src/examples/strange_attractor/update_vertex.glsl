f0a95b6568258be6674dff2bc3a143107a6669dc
Austin Theriot <austinmtheriot@gmail.com>
2021-12-22 13:31:31 -0600

    wrap particles to box

--- src/examples/strange_attractor/update_vertex.glsl
+++ src/examples/strange_attractor/update_vertex.glsl
@@ -7,5 +7,11 @@ out vec3 o_position;
 uniform float u_dt;
 
 void main() {
-  o_position = i_position + u_dt;
+  // update position
+  vec3 updated_position = i_position + 0.01;
+
+  // wrap position to box
+  updated_position = mod(updated_position + 1.0, 2.0) - 1.0;
+
+  o_position = updated_position;
 }

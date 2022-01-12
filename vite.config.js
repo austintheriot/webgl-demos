6e84a368822836c22d2f22f25df5caae60ed8161
Austin Theriot <austinmtheriot@gmail.com>
2022-01-12 12:47:03 -0600

    add camera with velocity to vite config

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ module.exports = defineConfig({
         main: resolve(__dirname, 'index.html'),
         strangeAttractors: resolve(__dirname, 'src/demos/strange_attractor/index.html'),
         parametricEquations: resolve(__dirname, 'src/demos/parametric_equations/index.html'),
+        cameraWithVelocity: resolve(__dirname, 'src/webglfundamentals/3d_camera_with_velocity/index.html'),
       }
     }
   }
